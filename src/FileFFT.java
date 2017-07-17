04f69ea1ffd9dbecf2028c172638a35a4947b4a6
Confrizz <belous.philip@gmail.com>
2017-07-17 10:05:02 -0600

    CalculateFFT

--- src/FileFFT.java
+++ src/FileFFT.java
@@ -7,11 +7,12 @@ import java.io.*;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 
-public class FileFFT implements Runnable{
+public class FileFFT {
     private File file = null;
     private AudioInputStream audioInputStream;
     private double[] data;
     private DoubleFFT_1D fft;
+    private int numOfSamples;
 
     public FileFFT(File file) throws IOException, UnsupportedAudioFileException {
         this.file = file;
@@ -30,24 +31,47 @@ public class FileFFT implements Runnable{
         }
 
         data = new double[bytes.length / 8];
+        double prevDataNum = 0;
+        boolean prevDataNan = false;
+
         for (int i = 0; i < data.length; i++) {
             data[i] = ByteBuffer.wrap(bytes, i * 8, 8).getDouble();
+
+            if (prevDataNan) {
+                data[i - 1] = (data[i - 2] + data[i]) /  2;
+            }
+
+            if (Double.isNaN(data[i])) {
+                prevDataNan = true;
+            } else {
+                prevDataNan = false;
+            }
+
+        }
+
+        if (Double.isNaN(data[data.length - 1])) {
+            data[data.length - 1] = data[data.length - 2];
         }
     }
 
-    private void calculateFFT() {
-        double[] fftData = new double[data.length * 2];
-        for (int i = 0; i < data.length; i++) {
-            fftData[2 * i] = data[i];
+    private void calculateFFT() throws IOException {
+        numOfSamples = 2048;
+        double[] fftData = new double[numOfSamples * 2];
+        for (int i = 0; i < numOfSamples; i++) {
+            fftData[2 * i] = (data[i] * 0.5 * (1.0 - Math.cos(2.0 * Math.PI * i / numOfSamples))) / Math.pow(10, 300); //Hann Window
             fftData[2 * i + 1] = 0;
         }
-        fft = new DoubleFFT_1D(data.length);
+        fft = new DoubleFFT_1D(numOfSamples);
         fft.complexForward(fftData);
+
+        BufferedWriter outputStream = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("FreqBands.txt"), "UTF-8"));
+        double[] frequencyBands = new double[3];
+        for (int i = 0; i < frequencyBands.length; i++) {
+            for (int j = 0; j < fftData.length; j += fftData.length / frequencyBands.length * i + 2) {
+                frequencyBands[i] += Math.sqrt(fftData[j] * fftData[j] + fftData[j + 1] * fftData[j + 1]) / 3;
+            }
+        }
+        outputStream.write(Arrays.toString(frequencyBands));
+        outputStream.flush();
     }
-
-    @Override
-    public void run() {
-
-    }
-
 }
--- src/PlayFFT.java
+++ src/PlayFFT.java
@@ -1,7 +1,15 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-/**
- * Created by Philip on 7/16/2017.
- */
 public class PlayFFT {
+    public PlayFFT() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("Freqbands.txt"));
+
+        char[] fileData = new char[100];
+        char c;
+//        while((c = bufferedReader.read(fileData)) != ",") {
+//
+//        }
+    }
 }
