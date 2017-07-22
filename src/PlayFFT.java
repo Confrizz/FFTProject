6e2a3a1c16906b17caecd24fb256bdda34dc5789
Confrizz <belous.philip@gmail.com>
2017-07-22 09:50:23 -0600

    PLaysFFT

--- src/PlayFFT.java
+++ src/PlayFFT.java
@@ -1,50 +1,57 @@
-import sun.audio.AudioData;
-import sun.audio.AudioDataStream;
-import sun.audio.AudioPlayer;
-
-import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class PlayFFT {
+public class PlayFFT implements Runnable{
     private File fftFile;
     private File audioFile;
     private int numOfSamples = FileFFT.getNumOfSamples();
 
-    public PlayFFT(File audioFile, File fftFile) throws IOException, UnsupportedAudioFileException, InterruptedException {
+    public PlayFFT(File audioFile, File fftFile) throws IOException, UnsupportedAudioFileException, InterruptedException, LineUnavailableException {
         this.audioFile = audioFile;
         this.fftFile = fftFile;
 
         play();
     }
 
-    private void play() throws IOException, UnsupportedAudioFileException, InterruptedException {
-        byte[] byteData = DoubleByteConverter.fileToByteArray(audioFile);
-        byte[] slicedData = new byte[numOfSamples * 8];
-
-        AudioDataStream audioStream;
+    private void play() throws IOException, UnsupportedAudioFileException, InterruptedException, LineUnavailableException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(fftFile));
+        int dataLength = DoubleByteConverter.fileToByteArray(audioFile).length;
 
         String line;
         double bandOne, bandTwo, bandThree;
         int comma;
         int counter = 0;
+
+        run();
+
         while ((line = bufferedReader.readLine()) != null) {
             bandOne = Double.parseDouble(line.substring(line.indexOf("[") + 1, (comma = line.indexOf(","))));
             bandTwo = Double.parseDouble(line.substring(comma + 1, (comma = line.indexOf(",", comma + 1))));
             bandThree = Double.parseDouble(line.substring(comma + 1, line.indexOf("]")));
 
-            for (int i = 0; i < numOfSamples * 8; i++) {
-                slicedData[i] = byteData[i + (numOfSamples * counter * 8)];
-            }
-            audioStream = new AudioDataStream(new AudioData(slicedData));
-            AudioPlayer.player.start(audioStream);
-            AudioPlayer.player.join();
+            System.out.println(bandOne + " " + bandTwo + " " + bandThree);
+            Thread.sleep(8 * numOfSamples * numOfSamples / dataLength);
 
             counter++;
         }
 
+    }
+
+    @Override
+    public void run() {
+        try {
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(audioFile));
+            clip.start();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        }
     }
 }
