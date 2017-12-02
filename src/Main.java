8f26a07d12a2c945f094135ca8c7f2b4854f86ff
Confrizz <belous.philip@gmail.com>
2017-12-01 20:32:01 -0700

    Comments deleted

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
 import java.io.File;
 
 public class Main {
@@ -10,19 +7,9 @@ public class Main {
     public static void main(String[] args) throws Exception {
         FileFFT fileFFT = new FileFFT(audio, 2048);
         PlayFFT playFFT = new PlayFFT(audio, fftFile);
-//        Thread playMusic = new Thread(playFFT);
-//        if (playMusic.isAlive()) {
-//            System.out.println("isAlive");
-//        }
 
-
-        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(audio);
-        AudioFormat audioFormat = audioInputStream.getFormat();
-        System.out.println(audioFormat.toString());
-//        Clip clip = AudioSystem.getClip();
-//        clip.open(AudioSystem.getAudioInputStream(audio));
-//        clip.start();
-//        Thread.sleep(10000);
-//        clip.close();
+//        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(audio);
+//        AudioFormat audioFormat = audioInputStream.getFormat();
+//        System.out.println(audioFormat.toString());
     }
 }
